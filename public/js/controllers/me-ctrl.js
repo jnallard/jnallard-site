8e886a5431f1e5f27a2e8cac3611837a1c756585
jnallard <jnallard@wpi.edu>
2017-05-08 20:02:23 -0400

    updates risk and me UI

--- public/js/controllers/me-ctrl.js
+++ public/js/controllers/me-ctrl.js
@@ -3,12 +3,13 @@
  */
 
 angular.module('RDash')
-    .controller('MeCtrl', ['$scope', '$cookieStore', 'backend', MeCtrl]);
+    .controller('MeCtrl', ['$scope', '$timeout', '$cookieStore', 'backend', MeCtrl]);
 
-function MeCtrl($scope, $cookieStore, backend) {
+function MeCtrl($scope, $timeout, $cookieStore, backend) {
 
 
     var self = this;
+    self.saving = false;
 
     self.init = function(){
       $scope.userPromise.then(function(me){
@@ -18,9 +19,14 @@ function MeCtrl($scope, $cookieStore, backend) {
     };
 
     self.save = function(){
+      self.saving = true;
       backend.post("/users/me", {"name": self.name, "picture": self.picture}).then(function(result){
+        $timeout( function(){
+          self.saving = false;
+        }, 1000 );
         $scope.updateUser();
       }).catch(function(error){
+        self.saving = false;
         console.log(error);
       });
     }
